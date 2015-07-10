ebe9f3d590c872b5b4d40d7609828f06c95002a0
lapiu <pipo2203@gmail.com>
2015-07-10 11:31:46 +0700

    First
    Web + Việt hóa

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/upload/admin/');
+define('HTTP_CATALOG', 'http://localhost/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/upload/admin/');
+define('HTTPS_CATALOG', 'http://localhost/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/upload/admin/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/upload/system/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/upload/admin/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/upload/admin/view/template/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/upload/system/config/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/upload/image/');
+define('DIR_CACHE', 'D:/xampp/htdocs/upload/system/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/upload/system/download/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/upload/system/upload/');
+define('DIR_LOGS', 'D:/xampp/htdocs/upload/system/logs/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/upload/system/modification/');
+define('DIR_CATALOG', 'D:/xampp/htdocs/upload/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/upload/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/upload/');
+
+// DIR
+define('DIR_APPLICATION', 'D:/xampp/htdocs/upload/catalog/');
+define('DIR_SYSTEM', 'D:/xampp/htdocs/upload/system/');
+define('DIR_LANGUAGE', 'D:/xampp/htdocs/upload/catalog/language/');
+define('DIR_TEMPLATE', 'D:/xampp/htdocs/upload/catalog/view/theme/');
+define('DIR_CONFIG', 'D:/xampp/htdocs/upload/system/config/');
+define('DIR_IMAGE', 'D:/xampp/htdocs/upload/image/');
+define('DIR_CACHE', 'D:/xampp/htdocs/upload/system/cache/');
+define('DIR_DOWNLOAD', 'D:/xampp/htdocs/upload/system/download/');
+define('DIR_UPLOAD', 'D:/xampp/htdocs/upload/system/upload/');
+define('DIR_MODIFICATION', 'D:/xampp/htdocs/upload/system/modification/');
+define('DIR_LOGS', 'D:/xampp/htdocs/upload/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', '');
